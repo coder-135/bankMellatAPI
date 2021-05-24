86e525f1be6a444517be9e1fd548ecee3f8999bc
Seyyed Reza <darkness4ever4@gmail.com>
2021-05-24 20:15:25 +0430

    changed method  API

--- apps/routes.js
+++ apps/routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require('./controller/controller');
 
 router.get('/mellat', controller.payment);
-router.get('callbackmellat', controller.callBackMellat);
+router.post('callbackmellat', controller.callBackMellat);
 
 module.exports = router;
